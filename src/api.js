0b5144cdd2e8afe01ff2d1e1312dee3896977a24
somang <somang0301@gmail.com>
2024-06-04 17:46:46 +0900

    handleLoadMore button add

--- src/api.js
+++ src/api.js
@@ -1,5 +1,12 @@
-export async function getReviews() {
-  const response = await fetch("https://learn.codeit.kr/0301/film-reviews/");
+export async function getReviews({
+  order = "createdAt",
+  offset = 0,
+  limit = 6,
+}) {
+  const query = `order=${order}&offset=${offset}&limit=${limit}`;
+  const response = await fetch(
+    `https://learn.codeit.kr/0301/film-reviews?${query}`
+  );
   const body = await response.json();
   return body;
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReviewList from "./ReviewList";
 import { getReviews } from "../api";
+
+const LIMIT = 6;
 
 function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
@@ -13,16 +17,30 @@ function App() {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
-  const handleLoadClick = async () => {
-    const { reviews } = await getReviews();
-    setItems(reviews);
+  const handleLoad = async (options) => {
+    const { reviews, paging } = await getReviews(options);
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems([...items, ...reviews]);
+    }
+    setOffset(options.offset + reviews.length);
+    setHasNext(paging.hasNext);
   };
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: LIMIT });
+  };
+
+  useEffect(() => {
+    handleLoad({ order, offset: 0, limit: LIMIT });
+  }, [order]);
+
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleBestClick}>베스트순</button>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      <button onClick={handleLoadClick}>불러오기</button>
+      {hasNext && <button onClick={handleLoadMore}>더 보기</button>}
     </div>
   );
 }
