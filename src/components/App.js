c7af96522967258c7d193325e1009f08f57eb409
somang <somang0301@gmail.com>
2024-06-05 12:14:47 +0900

    ReviewForm add

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import ReviewList from "./ReviewList";
 import { getReviews } from "../api";
+import ReviewForm from "./ReviewForm";
 
 const LIMIT = 6;
 
@@ -51,8 +52,11 @@ function App() {
 
   return (
     <div>
-      <button onClick={handleNewestClick}>최신순</button>
-      <button onClick={handleBestClick}>베스트순</button>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+      <ReviewForm />
       <ReviewList items={sortedItems} onDelete={handleDelete} />
       {hasNext && (
         <button disabled={isLoading} onClick={handleLoadMore}>
